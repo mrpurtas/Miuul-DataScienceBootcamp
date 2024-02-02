ba4f7cd9228c17c96cf5e2843ec1f8b448e71c90
mrpurtas <oguzhanpurtas46@gmail.com>
2024-02-02 17:48:45 +0300

    Tasks - List_CompExercises

---
+++ Tasks-MiniProjects/1st-week/list_comp_exercises.py
@@ -0,0 +1,117 @@
+import seaborn as sns
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+df = sns.load_dataset("car_crashes")
+df.columns
+df.info()
+##################################################
+# List Comprehensions
+##################################################
+
+# ###############################################
+# # GÖREV 1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini büyük harfe çeviriniz ve başına NUM ekleyiniz.
+# ###############################################
+df.columns
+# # Beklenen Çıktı
+#
+# # ['NUM_TOTAL',
+# #  'NUM_SPEEDING',
+# #  'NUM_ALCOHOL',
+# #  'NUM_NOT_DISTRACTED',
+# #  'NUM_NO_PREVIOUS',
+# #  'NUM_INS_PREMIUM',
+# #  'NUM_INS_LOSSES',
+# #  'ABBREV']
+#
+# # Notlar:
+# # Numerik olmayanların da isimleri büyümeli.
+# # Tek bir list comp yapısı ile yapılmalı.
+
+df.columns
+resultt = ["NUM_" + x.upper() if df else x.upper() for x in df.columns]
+print(resultt)
+
+
+
+
+
+
+resultt = ["NUM_" + col.upper() if pd.api.types.is_numeric_dtype(df[col]) else col.upper() for col in df.columns]
+print(resultt)
+
+import seaborn as sns
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+
+df = sns.load_dataset("car_crashes")
+df.columns
+df.info()
+
+
+
+
+# ###############################################
+# # GÖREV 2: List Comprehension yapısı kullanarak car_crashes verisindeki isminde "no" barındırmayan değişkenlerin isimlerininin sonuna "FLAG" yazınız.
+# ###############################################
+
+
+
+
+# # Notlar:
+# # Tüm değişken isimleri büyük olmalı.
+# # Tek bir list comp ile yapılmalı.
+#
+# # Beklenen çıktı:
+#
+# # ['TOTAL_FLAG',
+# #  'SPEEDING_FLAG',
+# #  'ALCOHOL_FLAG',
+# #  'NOT_DISTRACTED',
+# #  'NO_PREVIOUS',
+# #  'INS_PREMIUM_FLAG',
+# #  'INS_LOSSES_FLAG',
+# #  'ABBREV_FLAG']
+
+
+
+# ###############################################
+# # Görev 3: List Comprehension yapısı kullanarak aşağıda verilen değişken isimlerinden FARKLI olan değişkenlerin isimlerini seçiniz ve yeni bir dataframe oluşturunuz.
+# ###############################################
+df.columns
+og_list = ["abbrev", "no_previous"]
+
+new_cols = [for col in df.columns if col not in og_list]
+df[new_cols] = new_df
+new_df
+# # Notlar:
+# # Önce yukarıdaki listeye göre list comprehension kullanarak new_cols adında yeni liste oluşturunuz.
+# # Sonra df[new_cols] ile bu değişkenleri seçerek yeni bir df oluşturunuz adını new_df olarak isimlendiriniz.
+#
+# # Beklenen çıktı:
+#
+# #    total  speeding  alcohol  not_distracted  ins_premium  ins_losses
+# # 0 18.800     7.332    5.640          18.048      784.550     145.080
+# # 1 18.100     7.421    4.525          16.290     1053.480     133.930
+# # 2 18.600     6.510    5.208          15.624      899.470     110.350
+# # 3 22.400     4.032    5.824          21.056      827.340     142.390
+# # 4 12.000     4.200    3.360          10.920      878.410     165.630
+#
+
+
+
+def alternate(string):
+    new_str = ""
+    for index_info in range(len(string)):
+        if index_info % 2 == 0:
+            new_str += string[index_info].upper()
+        else:
+            new_str += string[index_info].lower()
+    print(new_str)
+
+
+alternate("merhabanaber")
